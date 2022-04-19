92d2f323e56519b239c882b493aa72ae2b9ed68f
Emmanuel Okiche <fleepgeek@gmail.com>
2022-04-19 16:05:50 +0100

    add student materials

---
+++ 20-navigate-to-the-about-page/Final/Bullseye/app/src/main/java/com/yourcompany/bullseye/AboutActivity.kt
@@ -0,0 +1,21 @@
+package com.yourcompany.bullseye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yourcompany.bullseye.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityAboutBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityAboutBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    title = getString(R.string.about_page_title)
+
+    binding.backButton?.setOnClickListener {
+      finish()
+    }
+  }
+}
